2d289b73a6617da28146a6380cb882176c9c235b
RobertaNetzova123 <roberta.netzova@gmail.com>
2017-06-05 21:35:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+            double course = 0;
+            if (p % n == 0)
+                course = p / n;
+            else
+            
+                course = (double) p / n;
+                Console.WriteLine( "{0:f3}",course);
+
+                Console.WriteLine("{0}", Math.Ceiling(course));
+            
+        }
+    }
+}
